9b1573e75c4aba1d300a83a43127803fc0d01121
chenrayray <57258764+chenrayray@users.noreply.github.com>
2019-12-12 14:02:24 +0800

    Add files via upload

---
+++ googleSheets.ino
@@ -0,0 +1,159 @@
+/*
+ * Rui Santos 
+ * Complete Project Details http://randomnerdtutorials.com
+ */
+ 
+//#ifdef ESP32
+#include <WiFi.h>
+#include <DHT.h>
+//#else
+//  #include <ESP8266WiFi.h>
+//#endif
+
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+
+// Replace with your SSID and Password
+const char* ssid     = "chenrayray";
+const char* password = "88888888";
+
+// Replace with your unique IFTTT URL resource
+const char* resource = "/trigger/UploadData/with/key/mQNp0HtPKYFJmdixkRZ2lwjZU1WVDjmWW2W2IrUJgxv";
+
+// How your resource variable should look like, but with your own API KEY (that API KEY below is just an example):
+//const char* resource = "/trigger/bme280_readings/with/key/nAZjOphL3d-ZO4N3k64-1A7gTlNSrxMJdmqy3";
+
+// Maker Webhooks IFTTT
+const char* server = "https://maker.ifttt.com";
+
+// Time to sleep
+uint64_t uS_TO_S_FACTOR = 1000000;  // Conversion factor for micro seconds to seconds
+// sleep for 30 minutes = 1800 seconds
+uint64_t TIME_TO_SLEEP = 1800;
+
+#define DHTPIN 16
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+String readDHTTemperature() {
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  //float t = dht.readTemperature(true);
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(t)) {    
+    Serial.println("Failed to read from DHT sensor!");
+    return "--";
+  }
+  else {
+    Serial.println(t);
+    return String(t);
+  }
+}
+
+String readDHTHumidity() {
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  if (isnan(h)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return "--";
+  }
+  else {
+    Serial.println(h);
+    return String(h);
+  }
+}
+
+void setup() {
+  Serial.begin(115200); 
+  delay(2000);
+
+  initWifi();
+  makeIFTTTRequest();
+    
+  #ifdef ESP32
+    // enable timer deep sleep
+    esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);    
+    Serial.println("Going to sleep now");
+    // start deep sleep for 3600 seconds (60 minutes)
+    esp_deep_sleep_start();
+  #else
+    // Deep sleep mode for 3600 seconds (60 minutes)
+    Serial.println("Going to sleep now");
+    ESP.deepSleep(TIME_TO_SLEEP * uS_TO_S_FACTOR); 
+  #endif
+}
+
+void loop() {
+  // sleeping so wont get here 
+}
+
+// Establish a Wi-Fi connection with your router
+void initWifi() {
+  Serial.print("Connecting to: "); 
+  Serial.print(ssid);
+  WiFi.begin(ssid, password);  
+
+  int timeout = 10 * 4; // 10 seconds
+  while(WiFi.status() != WL_CONNECTED  && (timeout-- > 0)) {
+    delay(250);
+    Serial.print(".");
+  }
+  Serial.println("");
+
+  if(WiFi.status() != WL_CONNECTED) {
+     Serial.println("Failed to connect, going back to sleep");
+  }
+
+  Serial.print("WiFi connected in: "); 
+  Serial.print(millis());
+  Serial.print(", IP address: "); 
+  Serial.println(WiFi.localIP());
+}
+
+// Make an HTTP request to the IFTTT web service
+void makeIFTTTRequest() {
+  Serial.print("Connecting to "); 
+  Serial.print(server);
+  
+  WiFiClient client;
+  int retries = 20;
+  while(!!!client.connect(server, 80) && (retries-- > 0)) {
+    Serial.print(".");
+  }
+  Serial.println();
+  if(!!!client.connected()) {
+    Serial.println("Failed to connect...");
+  }
+  
+  Serial.print("Request resource: "); 
+  Serial.println(resource);
+
+  // Temperature in Celsius
+  String jsonObject = String("{\"value1\":\"") + readDHTTemperature() + "\",\"value2\":\"" + "rui"
+                      + "\",\"value3\":\"" + readDHTHumidity() + "\"}";
+                                         
+  client.println(String("POST ") + resource + " HTTP/1.1");
+  client.println(String("Host: ") + server); 
+  client.println("Connection: close\r\nContent-Type: application/json");
+  client.print("Content-Length: ");
+  client.println(jsonObject.length());
+  client.println();
+  client.println(jsonObject);
+        
+  int timeout = 5 * 10; // 5 seconds             
+  while(!!!client.available() && (timeout-- > 0)){
+    delay(100);
+  }
+  if(!!!client.available()) {
+    Serial.println("No response...");
+  }
+  while(client.available()){
+    Serial.write(client.read());
+  }
+  
+  Serial.println("\nclosing connection");
+  client.stop(); 
+}
